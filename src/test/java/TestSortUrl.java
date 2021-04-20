a9e4db77425599b9472af9a8d8adf1784a138e60
KBdog <1246450339@qq.com>
2021-04-20 18:56:43 +0800

    补充部分内容

--- src/test/java/TestSortUrl.java
+++ src/test/java/TestSortUrl.java
@@ -16,6 +16,12 @@ public class TestSortUrl {
         Proxy proxy=new Proxy(Proxy.Type.HTTP,new InetSocketAddress("127.0.0.1",1080));
         CopyMangaParser parser=new CopyMangaParserImpl();
         List<ComicUrl> list = parser.getUrlList("tayutadexuanze", "5847ba3c-88ae-11ea-90aa-00163e0ca5bd", proxy);
+        //正序
+//        list.sort(Comparator.comparing(ComicUrl::getUrlId));
+        //倒序
         list.sort(Comparator.comparing(ComicUrl::getUrlId).reversed());
+        for(ComicUrl url:list){
+            System.out.println(url.getUrlId()+"--"+url.getUrlAddress());
+        }
     }
 }
